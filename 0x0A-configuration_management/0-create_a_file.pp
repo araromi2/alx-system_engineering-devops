efb279a1a7ab4df4023056fb30c19bc8f909e44e
araromi2 <abdulhameedararomi@gmail.com>
2023-03-25 07:44:31 -0700

    Add 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This Puppet manifest creates a file in /tmp/school
+file {'/tmp/school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
