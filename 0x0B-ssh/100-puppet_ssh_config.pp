13f1a678635e5cfcb71d0a00dbfa3c450db9866b
araromi2 <abdulhameedararomi@gmail.com>
2023-03-28 09:53:02 -0700

    Configure the SSH client using puppet to set the HostName, ServerAliveInterval, IdentityFile, and PasswordAuthentication options in the /etc/ssh/ssh_config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,14 @@
-file { '/root/.ssh/config':
-  ensure => present,
-  mode   => '0600',
-  content => "Host example.com\n\
-              IdentityFile ~/.ssh/school\n\
-              PasswordAuthentication no\n",
+# Configuring the SSH client using puppet 
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content =>
+    "Host school
+        HostName 52.205.88.16
+        ServerAliveInterval 120
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no",
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0744'
 }
