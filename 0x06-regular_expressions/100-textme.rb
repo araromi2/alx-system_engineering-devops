1fedbe8b7442f60c1095301a5568353ac91b3030
abdulhameed <abdulhameedararomi@gmail.com>
2023-05-04 00:42:49 -0700

    Add Ruby script to extract information from a log file

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+log_line = ARGV[0]
+sender = log_line.scan(/(?<=from:)[^\]]*/).join
+receiver = log_line.scan(/(?<=to:)[^\]]*/).join
+flags = log_line.scan(/(?<=flags:)[^\]]*/).join
+puts "#{sender}, #{receiver}, #{flags}"
