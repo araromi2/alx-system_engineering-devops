0476b28012622f75ce712907c4471a92c2503df1
abdulhameed <abdulhameedararomi@gmail.com>
2023-05-04 03:59:48 -0700

    Update the Ruby script to pass the test

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,4 +3,4 @@ log_line = ARGV[0]
 sender = log_line.scan(/(?<=from:)[^\]]+/).join
 receiver = log_line.scan(/(?<=to:)[^\]]+/).join
 flags = log_line.scan(/(?<=flags:)[^\]]+/).join
-puts "#{sender}, #{receiver}, #{flags}"
+puts "#{sender},#{receiver},#{flags}"
